2a4f5f3be930c98ea045ef0375a0e2003389f168
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-02-06 18:14:22 -0500

    commit tree d158e55f32117e6645f5d632c0f78d393d40b067

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return(`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(work = "go to the office") {
+    return (`This Monday, I will ${work}.`);
+}
+
+function wrapAdjective(string = "*") {
+     return function(defaulted = "special"){
+        return `You are ${string}${defaulted}${string}!`;
+    }}
+   
+
+
+
+// describe("defines wrapAdjective function according to the specification", function() {
+//     it("function exists", function() {
+//       expect(wrapAdjective).to.exist
+//     })
+    
+//     it("when initialized with '*' creates a function that, when called, wraps an adjective in a highlight", function() {
+//       let result = wrapAdjective('*')
+//       let emphatic = result("a hard worker")
+//       expect(emphatic).to.equal("You are *a hard worker*!")
+//     });
+
+//     it("when initialized with '||' creates a function that, when called, wraps an adjective in a highlight", function() {
+//       let result = wrapAdjective("||")
+//       let emphatic = result("a dedicated programmer")
+//       expect(emphatic).to.equal("You are ||a dedicated programmer||!")
+//     });
+//   })
+// })
